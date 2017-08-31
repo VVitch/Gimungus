baffc1900f7d2a88268ae98ce214cacbd7017f4e
astralsquid <Brewmaster@brewmaster.trinity.local>
2017-08-30 20:15:50 -0500

    First Commit

---
+++ Assets/PlayerInputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour {
+	public Unit playerUnit;
+	public Camera mainCamera;
+	float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float hMovement = Input.GetAxis("Horizontal") * speed;
+		float vMovement = Input.GetAxis ("Vertical") * speed;
+
+		playerUnit.transform.Translate (hMovement, vMovement, 0);
+		mainCamera.transform.Translate (hMovement, vMovement, 0);
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	// Use this for initialization
+	protected void Start () {
+		
+	}
+
+	// Update is called once per frame
+	protected void Update () {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+}
